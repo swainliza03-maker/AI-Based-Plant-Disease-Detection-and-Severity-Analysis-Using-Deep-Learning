fb8dab820f6185e42fe7e7f634a908a9ddc9cde8
Liza Swain <swainliza03@gmail.com>
2026-09-23 12:08:42 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.title(" Plant Disease Detection")
+
+st.write(
+    "AI-Based Plant Disease Detection and Severity Analysis "
+    "Using Deep Learning"
+)
+
+uploaded_file = st.file_uploader(
+    "Upload a plant leaf image",
+    type=["jpg", "jpeg", "png"]
+)
+
+if uploaded_file is not None:
+    st.image(
+        uploaded_file,
+        caption="Uploaded Leaf Image",
+        use_container_width=True
+    )
+
+    st.success("Image uploaded successfully!")
