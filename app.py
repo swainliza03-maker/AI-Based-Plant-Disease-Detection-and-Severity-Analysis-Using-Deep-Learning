e91eb2fee924caa30280b4ed5075df010aba088e
Liza Swain <swainliza03@gmail.com>
2026-09-23 12:10:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
 
 st.title(" Plant Disease Detection")
 
@@ -7,16 +10,44 @@ st.write(
     "Using Deep Learning"
 )
 
+# Load trained model from Hugging Face
+MODEL_URL = "https://huggingface.co/Liza-Swain/Plant-disease-model/resolve/main/plant_disease_model%20%281%29.keras"
+
+@st.cache_resource
+def load_model():
+    return tf.keras.models.load_model(MODEL_URL)
+
+model = load_model()
+
 uploaded_file = st.file_uploader(
     "Upload a plant leaf image",
     type=["jpg", "jpeg", "png"]
 )
 
 if uploaded_file is not None:
+
+    image = Image.open(uploaded_file).convert("RGB")
+
     st.image(
-        uploaded_file,
+        image,
         caption="Uploaded Leaf Image",
         use_container_width=True
     )
 
-    st.success("Image uploaded successfully!")
+    # Resize image
+    image = image.resize((128, 128))
+
+    # Convert to array
+    image_array = np.array(image) / 255.0
+
+    # Add batch dimension
+    image_array = np.expand_dims(image_array, axis=0)
+
+    # Prediction
+    prediction = model.predict(image_array)
+
+    predicted_class = np.argmax(prediction[0])
+    confidence = np.max(prediction[0]) * 100
+
+    st.success(f"Predicted Class: {predicted_class}")
+    st.info(f"Confidence: {confidence:.2f}%")
